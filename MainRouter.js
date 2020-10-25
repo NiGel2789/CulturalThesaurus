c42dad8a47abddadbc8fc40a2f04b038ecdadf44
henryhan-lab <59583205+henryhan-lab@users.noreply.github.com>
2020-10-25 10:23:20 -0700

    Add files via upload

---
+++ MainRouter.js
@@ -0,0 +1,36 @@
+import React, {Component} from 'react'
+import {Route, Switch} from 'react-router-dom'
+import Home from './Core/Home'
+import SignIn from './SignIn/signin'
+import SignUp from './SignIn/signup'
+import User from './User/user'
+import About from './About/about'
+import createWord from './Post/createWord'
+import readWord from './Post/readWord'
+import updateWord from './Post/updateWord'
+import deleteWord from './Post/deleteWord'
+import Menu from './Core/Menu'
+import SearchAppBar from './Core/search'
+const MainRouter = () => {
+    
+    return (<div>
+
+      <Menu></Menu>
+      <SearchAppBar></SearchAppBar>
+      
+      <Switch>
+        <Route exact path="/home" component={Home}/>
+        <Route exact path="/signin" component={SignIn}/>
+        <Route exact path="/signup" component={SignUp}/>
+        <Route exact path="/user" component={User}/>
+        <Route exact path="/about" component={About}/>
+        <Route exact path="/createword" component={createWord}/>
+        <Route exact path="/readword" component={readWord}/>
+        <Route exact path="/updateword" component={updateWord}/>
+        <Route exact path="/deleteword" component={deleteWord}/>
+
+      </Switch>
+    </div>)
+}
+
+export default MainRouter
