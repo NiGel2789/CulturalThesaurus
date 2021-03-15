5b891d90a0aad4e67c38f7efb162c178286eda8d
dreadwuulf <46611686+dreadwuulf@users.noreply.github.com>
2021-03-14 21:14:57 -0700

    giphy app

--- src/Post/giphyapp.js
+++ src/Post/giphyapp.js
@@ -3,6 +3,6 @@ import { GiphyFetch } from '@giphy/js-fetch-api'
 // use @giphy/js-fetch-api to fetch gifs, instantiate with your api key
 const gf = new GiphyFetch(UhFUfdL0oZHHh20DVt0ztFH6GbBYnZov)
 // configure your fetch: fetch 10 gifs at a time as the user scrolls (offset is handled by the grid)
-const fetchGifs = (offset: number) => gf.trending({ offset, limit: 10 })
+//const fetchGifs = (offset: number) => gf.trending({ offset, limit: 10 })
 // Render the React Component and pass it your fetchGifs as a prop
-ReactDOM.render(<Grid width={800} columns={3} fetchGifs={fetchGifs} />, target)
+//ReactDOM.render(<Grid width={800} columns={3} fetchGifs={fetchGifs} />, target)
